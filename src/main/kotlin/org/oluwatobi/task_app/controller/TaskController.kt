3568a07e7250abd8882ddfd496f2fd68cbb0e79b
Oluwatobi Abass <oluwatobiabass5@gmail.com>
2024-01-30 02:39:39 +0100

    created the task controller and also handled the cors configuration

---
+++ src/main/kotlin/org/oluwatobi/task_app/controller/TaskController.kt
@@ -0,0 +1,42 @@
+package org.oluwatobi.task_app.controller
+
+import jakarta.validation.Valid
+import org.oluwatobi.task_app.data.model.TaskCreateRequest
+import org.oluwatobi.task_app.data.model.TaskDto
+import org.oluwatobi.task_app.data.model.TaskUpdateRequest
+import org.oluwatobi.task_app.service.TaskService
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.*
+
+@RestController
+@RequestMapping("api")
+class TaskController(private val service: TaskService) {
+
+    @GetMapping("all-tasks")
+    fun getAllTasks(): ResponseEntity<List<TaskDto>> = ResponseEntity(service.getAllTasks(), HttpStatus.OK)
+
+    @GetMapping("open-tasks")
+    fun getAllOpenTasks(): ResponseEntity<List<TaskDto>> = ResponseEntity(service.getAllOpenTasks(), HttpStatus.OK)
+
+    @GetMapping("closed-tasks")
+    fun getAllClosedTasks(): ResponseEntity<List<TaskDto>> = ResponseEntity(service.getAllClosedTasks(), HttpStatus.OK)
+
+    @GetMapping("task/{id}")
+    fun getTaskById(@PathVariable id: Long): ResponseEntity<TaskDto> =
+        ResponseEntity(service.getTaskById(id), HttpStatus.OK)
+
+    @PostMapping("create")
+    fun createTask(@Valid @RequestBody request: TaskCreateRequest): ResponseEntity<TaskDto> =
+        ResponseEntity(service.createTask(request), HttpStatus.OK)
+
+    @PostMapping("update/{id}")
+    fun updateTask(
+        @PathVariable id: Long,
+        @Valid @RequestBody request: TaskUpdateRequest
+    ): ResponseEntity<TaskDto> = ResponseEntity(service.updateTask(id, request), HttpStatus.OK)
+
+    @DeleteMapping("delete/{id}")
+    fun deleteTask(@PathVariable id: Long): ResponseEntity<String> =
+        ResponseEntity(service.deleteTask(id), HttpStatus.OK)
+}
--- src/main/kotlin/org/oluwatobi/task_app/service/TaskService.kt
+++ src/main/kotlin/org/oluwatobi/task_app/service/TaskService.kt
@@ -3,11 +3,15 @@ package org.oluwatobi.task_app.service
 import org.oluwatobi.task_app.data.Task
 import org.oluwatobi.task_app.data.model.TaskCreateRequest
 import org.oluwatobi.task_app.data.model.TaskDto
+import org.oluwatobi.task_app.data.model.TaskUpdateRequest
+import org.oluwatobi.task_app.exception.BadRequestException
 import org.oluwatobi.task_app.exception.TaskNotFoundException
 import org.oluwatobi.task_app.repository.TaskRepository
-import org.springframework.data.crossstore.ChangeSetPersister
 import org.springframework.stereotype.Service
+import org.springframework.util.ReflectionUtils
+import java.lang.reflect.Field
 import java.util.stream.Collectors
+import kotlin.reflect.full.memberProperties
 
 @Service
 class TaskService(private val taskRepository: TaskRepository) {
@@ -49,4 +53,40 @@ class TaskService(private val taskRepository: TaskRepository) {
 
     fun getAllClosedTasks(): List<TaskDto> =
         taskRepository.queryAllClosedTasks().stream().map(this::mappingEntityToDto).collect(Collectors.toList())
+
+    fun createTask(request: TaskCreateRequest): TaskDto {
+        if (taskRepository.doesDescriptionExist(request.description)) {
+            throw BadRequestException("There is already a task with the description: ${request.description}")
+        }
+
+        val task = Task()
+        mappingFromRequestToEntity(task, request)
+        val savedTask: Task = taskRepository.save(task)
+        return mappingEntityToDto(savedTask)
+    }
+
+    fun updateTask(id: Long, request: TaskUpdateRequest): TaskDto {
+        checkTaskForId(id)
+        val existingTask: Task = taskRepository.findTaskById(id)
+
+        for (prop in TaskUpdateRequest::class.memberProperties) {
+            if (prop.get(request) != null) {
+                val field: Field? = ReflectionUtils.findField(Task::class.java, prop.name)
+                field?.let {
+                    it.isAccessible = true
+                    ReflectionUtils.setField(it, existingTask, prop.get(request))
+                }
+            }
+        }
+
+        val savedTask: Task = taskRepository.save(existingTask)
+        return mappingEntityToDto(savedTask)
+    }
+
+    fun deleteTask(id: Long): String {
+        checkTaskForId(id)
+        taskRepository.deleteById(id)
+        return "Task with the ID: $id has been deleted."
+    }
+
 }
